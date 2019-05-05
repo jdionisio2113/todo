8843ee46fd5e012ad48af8f367c9f44a0cd6409a
Justin Dionisio <justindionisio21@yahoo.com>
2019-05-04 21:53:50 -0700

    Created todos component, added materialize.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React from "react";
+import Todos from "./Todos";
 
 class App extends React.Component {
-  state = {
-    todos: [
-      { id: 1, content: "buy some milk" },
-      { id: 2, content: "play mario kart" }
-    ]
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { id: 1, content: "buy some milk" },
+        { id: 2, content: "play mario kart" }
+      ]
+    };
+  }
 
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} />
+      </div>
+    );
   }
 }
 
