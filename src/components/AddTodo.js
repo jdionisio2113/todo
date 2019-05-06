d42f6710531e2ecac72c0f333a704754c7b92e01
Justin Dionisio <justindionisio21@yahoo.com>
2019-05-05 20:11:27 -0700

    Modified user input to add 'todos'

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -20,7 +20,10 @@ class AddTodo extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    console.log(this.state);
+    this.props.addTodo(this.state);
+    this.setState({
+      content: ""
+    });
   }
 
   render() {
@@ -28,7 +31,11 @@ class AddTodo extends React.Component {
       <div>
         <form onSubmit={this.handleSubmit}>
           <label>Add new todo</label>
-          <input type="text" onChange={this.handleChange} />
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.content}
+          />
         </form>
       </div>
     );
--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
     };
 
     this.deleteTodo = this.deleteTodo.bind(this);
+    this.addTodo = this.addTodo.bind(this);
   }
 
   deleteTodo(id) {
@@ -24,12 +25,20 @@ class App extends React.Component {
     });
   }
 
+  addTodo(todo) {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos
+    });
+  }
+
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todos</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
-        <AddTodo />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
